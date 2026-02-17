fda038f72336b47d1a5ff4a7bfa802919ee20f9e
Ubuntu <mog@forskningsradet.no>
2026-02-17 16:35:07 +0000

    changed function name

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 # __init__.py
 
-from .sdg_search import search_all_targets_in_goal, search_all_goals, run_all_country_searches
+from .sdg_search import search_in_text_for_one_sdg, search_all_goals, run_all_country_searches
 from .search_dataframe import dataframe_search
