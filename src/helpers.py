28b7fb0059e2bf57222e39dad985f1235d0ab472
Ubuntu <mog@forskningsradet.no>
2026-02-19 11:55:54 +0000

    fixing typehints

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,7 @@
 import re
 import os
 import json
+from typing import Any
 
 from .format_helpers import prepare_regex_search_termlist, format_logic_rules
 
@@ -15,7 +16,7 @@ def get_sdg_phrases(sdg_number: int) -> tuple[list[dict], list[dict], dict]:
     Returns:
         List of the presearches for the sdg, each a dictionary with the phrases' termlists and logic rule
         A list with dictionaries of all the phrases' termlists and logic rule for each of the SDG targets
-        A dict for the mentrions search, containing a logic rule and termlists
+        A dict for the mentions search, containing a logic rule and termlists
     """
     file_path_absolute = os.path.join(
         os.path.dirname(__file__), 'searchterms/sdg{}.json'.format(str(sdg_number))
@@ -31,9 +32,9 @@ def get_sdg_phrases(sdg_number: int) -> tuple[list[dict], list[dict], dict]:
         return [], targets, data['mentions']
 
 
-####################### search helpers for boolean/unindexed search #######################
+#################### Search helpers for boolean/unindexed search ####################
 def _pattern_search_boolean(regex_search: str, text: str) -> bool:
-    """search for regex pattern in a text.
+    """Search for regex pattern in a text.
 
     Args:
         regex_search: formated regex pattern
@@ -48,14 +49,16 @@ def _pattern_search_boolean(regex_search: str, text: str) -> bool:
 def _is_logic_rule_true(
         termlist_results, 
         logic_rule_raw, 
-        countries_results: dict[str:bool] = None,
-        pre_search_results: dict[str:bool] = None,
+        countries_results: dict[str, bool] = {},
+        pre_search_results: dict[str, bool] = {},
     ) -> bool:
     """Finds the result of a phrases logic rule given all the termlist results
 
     Args:
-        all_search_results: all the boolean results for the searches performed for each phrase
-        search_phrases: dictionary containing all data for all the phrases
+        termlist_results: all the boolean results for each termlist
+        logic_rule_raw: the logic rule to be evaluated
+        countries_results: 
+        pre_search_results: 
 
     Returns:
         the boolean result for the search
@@ -91,8 +94,8 @@ def search_for_phrase_unindexed(
         termlists: list[dict], 
         input_text: str, 
         logic_rule_raw: str, 
-        countries_results: dict[str:bool] = None,
-        pre_search_results: dict[str:bool] = None,
+        countries_results: dict[str, bool] = {},
+        pre_search_results: dict[str, bool] = {},
     ) -> bool:
     """Search all the termlists in a phrase and checks the logic rule
 
@@ -109,60 +112,21 @@ def search_for_phrase_unindexed(
     return _is_logic_rule_true(termlist_results, logic_rule_raw, countries_results, pre_search_results)
 
 
-def run_all_targets_in_goal_search(
-        sdg_all_targets: dict,
-        input_text: str,
-        countries_result: dict,
-        pre_search_result: dict,
-        get_indexes: bool = False
-    ):
-    """_summary_
-
-    Args:
-        sdg_all_targets: _description_
-        input_text: _description_
-        countries_result: _description_
-        pre_search_result: _description_
-        get_indexes: _description_. Defaults to False.
-
-    Returns:
-        _description_
-    """
-    results = {}
-    indexes = {}
-    for target in sdg_all_targets:
-        phrases_results = run_all_termlist_searches_in_phrase_bool(
-                    target['phrases'],
-                    input_text,
-                    'number',
-                    countries_results=countries_result,
-                    pre_search_results=pre_search_result,
-                )
-
-        if get_indexes:
-            index_search = search_phrases_in_sdg_target_indexed(
-                input_text, target, indexed=True
-            )
-            indexes[target['name']] = index_search
-        
-        results[target['name']] = phrases_results 
-    return results, indexes
-
-
-def run_all_termlist_searches_in_phrase_bool(
+def run_all_termlist_searches_in_list_of_phrases_bool(
     search_phrases: list[dict], 
     input_text: str, 
     name_key: str, 
-    countries_results: dict[str:bool] = None,
-    pre_search_results: dict[str:bool] = None,
-) -> dict[str : dict[str:bool]]:
+    countries_results: dict[str, bool] = {},
+    pre_search_results: dict[str, bool] = {},
+) -> dict[Any, bool]:
     """Perform a boolean search for all phrases in a dictionary of phrases on a given text
 
     Args:
         search_phrases: all the searches to perform
         input_text: the text to search in
         name_key: name of the key in a phrase dict indicating the name or number of the phrase
-        indexed: whether to include indexed search or not
+        countries_results: 
+        pre_search_results: a dict withbool results for each presearch in the p
 
     Returns:
         the results of all the searches
@@ -204,92 +168,34 @@ def run_all_termlist_searches_in_phrase_bool(
     return all_search_results
 
 
-################### Search helpers for indexed search ######################################
-def _pattern_search_indexed(regex_search: str, text: str) -> list[dict]:
-    """Find instances of words from a list in a text, including where in the text the search terms were
+def run_all_targets_in_goal_search(
+        sdg_all_targets: list[dict],
+        input_text: str,
+        countries_result: dict[str, bool],
+        pre_search_result: dict[str, bool],
+    ) -> dict[str, dict[str, bool]]:
+    """_summary_
 
     Args:
-        regex_search: formated regex pattern
-        text: the text to search in
+        sdg_all_targets: _description_
+        input_text: _description_
+        countries_result: _description_
+        pre_search_result: _description_
 
     Returns:
-        a list of all matches contaiting a dict of the matched text along with its start and end index in the text
+        _description_
     """
-    matches = re.finditer(rf'{regex_search}', text)
+    results = {}
 
-    matches_with_indices = []
-    for match in matches:
-        matches_with_indices.append(
-            {
-                'matched_text': match.group(0),
-                'start_index': match.start(),
-                'end_index': match.end(),
-            }
-        )
+    for target in sdg_all_targets:
+        phrases_results = run_all_termlist_searches_in_list_of_phrases_bool(
+                    target['phrases'],
+                    input_text,
+                    'number',
+                    countries_results=countries_result,
+                    pre_search_results=pre_search_result,
+                )
+        results[target['name']] = phrases_results 
 
-    return matches_with_indices
+    return results
 
-
-def search_phrases_in_sdg_target_indexed(
-    input_text: str, 
-    sdg_target_phrases: list[dict], 
-    key_name: str,
-    countries_results: dict[str:bool] = None,
-    pre_search_results: dict[str:bool] = None,
-) -> dict[int, dict]:
-    """Search for all phrases in a target for a text
-
-    Args:
-        input_text: the text to search in
-        sdg_target_phrases: phrases with the logic rules to evaluate
-        indexed: wether to include indexed results or not
-
-    Returns:
-        the results for all termlist searches in each phrase
-    """
-    all_results = {}
-
-    for phrase in sdg_target_phrases:
-        logic_rule_raw = phrase['logic_rule']
-        phrase_results = {}
-        name = phrase[key_name]
-
-        sentence_split = False
-        if 'sentence_split' in phrase.keys():
-            if phrase['sentence_split'] == 'True':
-                sentence_split = True
-
-        if sentence_split:   
-            sentences = input_text.split(". ")
-            phrase_sentence_results = []
-            for sentence in sentences: 
-                termlist_results = {}
-                #TODO do it indexed!
-                sentence_result = search_for_phrase_unindexed(
-                    phrase['termlists'], 
-                    sentence, 
-                    logic_rule_raw,
-                    countries_results, 
-                    pre_search_results
-                    )
-                #TODO something like this: (både her og når det ikke er sentence_split)
-                for term_list in phrase['term_lists']:
-                    regex_term_list, formatted_text = prepare_regex_search_termlist(term_list, input_text)
-                    termlist_results[term_list['termlist_name']] = _pattern_search_indexed(regex_term_list, formatted_text)
-        
-                phrase_sentence_results.append(sentence_result)
-            all_results[name] = any(phrase_sentence_results)
-        
-        else: 
-            #TODO do it indexed!
-            all_results[name] = search_for_phrase_unindexed(
-                phrase['termlists'], 
-                input_text, 
-                logic_rule_raw,
-                countries_results, 
-                pre_search_results
-            )
-
-        all_results[name] = phrase_results
-
-    return all_results
--- src/sdg_search.py
+++ src/sdg_search.py
@@ -1,14 +1,15 @@
+from typing import Any
+
 from .helpers import (
     get_sdg_phrases,
     search_for_phrase_unindexed,
-    search_phrases_in_sdg_target_indexed,
     run_all_targets_in_goal_search,
 )
 from .consts import LIST_ALL_SDG_NR, COUNTRIES
 
 
 def run_goal_mentions_search(mentions_search: dict, input_text: str) -> bool:
-    """Run the mentions search for an sdg
+    """Run the mentions search for an sdg on a text
 
     Args:
         mentions_search: a dictionary with the logic rule and the termlists to search
@@ -28,14 +29,14 @@ def run_goal_mentions_search(mentions_search: dict, input_text: str) -> bool:
 
 def run_all_country_searches(
     input_text: str,
-) -> dict[str:bool]:
+) -> dict[str, bool]:
     """Triggers all country category searches for a text
 
     Args:
         input_text: The text to search in
 
     Returns:
-        A dictionary with the results for each country category and for each termlist in the country categories
+        A dictionary with the results for each country category
     """
     results = {}
     for country_search in COUNTRIES:
@@ -51,14 +52,15 @@ def run_all_country_searches(
 def run_pre_searches(
     pre_searches: list[dict],
     input_text: str,
-) -> dict[str:bool]:
+) -> dict[str, bool]:
     """Triggers all country category searches for a text
 
     Args:
+        pre_searches: the phrases for all the presearches to run
         input_text: The text to search in
 
     Returns:
-        A dictionary with the results for each country category and for each termlist in the country categories
+        A dictionary with the results for each pre search
     """
     results = {}
     for search in pre_searches:
@@ -69,29 +71,13 @@ def run_pre_searches(
                 )
 
     return results
-
-
-def get_logic_rule_raw(goal_phrases: dict[str, dict], phrase_nr: str) -> str:
-    """Get the logic rules of an SDG target for one specific phrase
-
-    Args:
-        goal_phrases: the whole phrases dict for an sdg target
-        phrase_nr: the phrase to get the logic rule for
-
-    Returns:
-        the raw logic rule of a search phrase
-    """
-    for phrase in goal_phrases:
-        if phrase["number"] == phrase_nr:
-            return phrase['logic_rule']
         
 
 def search_all_targets_in_goal(
     sdg_nr: int,
     input_text: str,
     countries: dict[str, bool],    
-    get_indexes: bool = False,
-) -> tuple[dict[str, bool], dict[str, dict]]:
+) -> dict[str, Any]:
     """Perform search on all targets in a goal for a given text
 
     NOTE: The indexed result is per termlist, and will also include terms that are matched in a termlist even if the logic rule gives False as a whole
@@ -99,71 +85,62 @@ def search_all_targets_in_goal(
     Args:
         sdg_nr: The number of sdg to perform a search for
         input_text: The text to perform the search on
-        analyze_text: boolean for whether to run an extra search that returns the indexes of words found for all search terms.
         countries: result of country searches (optional)
 
     Returns:
         The results in boolean for each target of the sdg
-        The indexed results for all search terms in each target of the sdg.
     """
-    results = {}
-    results["sdg_number"] = sdg_nr
+    results: dict[str, Any] = {}
+    results['sdg_number'] = sdg_nr
 
     pre_searches, sdg_all_targets, mentions_search = get_sdg_phrases(sdg_nr)
     
     pre_search_result = run_pre_searches(pre_searches, input_text)
-    results["pre_search"] = pre_search_result
+    results['pre_search'] = pre_search_result
 
-    results["targets"], indexes = run_all_targets_in_goal_search(sdg_all_targets, input_text, countries, pre_search_result)
+    results['targets'] = run_all_targets_in_goal_search(sdg_all_targets, input_text, countries, pre_search_result)
 
-    results["mentions"] = run_goal_mentions_search(mentions_search, input_text)
+    results['mentions'] = run_goal_mentions_search(mentions_search, input_text)
 
-    return results, indexes
+    return results
 
 
 def search_in_text_for_one_sdg(
         sdg_nr: int,
         input_text: str,
-        get_indexes: bool = False, 
-    ) -> tuple[dict[str, bool], dict[str, dict]]:
-    """_summary_
+    ) -> dict[str, Any]:
+    """performs the search for one sdg on one text
 
     Args:
-        sdg_nr: _description_
-        input_text: _description_
-        get_indexes: _description_. Defaults to False.
+        sdg_nr: the SDG to search for
+        input_text: the text to search in
 
     Returns:
-        _description_
+        the results for the sdg search, pre-search, targets and mentions
     """
     countries = run_all_country_searches(input_text)
-    results, indexes = search_all_targets_in_goal(
-        sdg_nr,
-        input_text,
-        countries,    
-        get_indexes
-    )
-    return results, indexes
-    
+    results = search_all_targets_in_goal(sdg_nr, input_text, countries)
+    return results
 
-def search_all_goals(text: str, sdg_list: list[int] = LIST_ALL_SDG_NR) -> dict:
+
+def search_all_goals(text: str, sdg_list: list[int] = LIST_ALL_SDG_NR) -> dict[str, dict[str, Any]]:
     """Search for all the goals in a text
 
     Args:
-        text: The text to perform the search on
-        sdg_list: list of sdgs to search for
+        text: the text to perform the search on
+        sdg_list: list of sdgs to search for, defaults to the const list of all SDGs
 
     Returns:
-        a dictionary with the results for country search and the SDG search results
+        the results for country search and the SDG search results
     """
-    results = {}
+    results: dict[str, Any] = {}
 
     countries = run_all_country_searches(text)
-    results["countries"] = countries
+    results['countries'] = countries
 
-    for sdg in [str(sdg) for sdg in sdg_list if sdg in set(LIST_ALL_SDG_NR)]:
-        sdg_result, _ = search_all_targets_in_goal(sdg, text, countries=countries)
-        results[sdg] = sdg_result
+    for sdg in [sdg for sdg in sdg_list if sdg in set(LIST_ALL_SDG_NR)]:
+        sdg_result = search_all_targets_in_goal(sdg, text, countries=countries)
+        results[str(sdg)] = sdg_result
 
     return results
 
