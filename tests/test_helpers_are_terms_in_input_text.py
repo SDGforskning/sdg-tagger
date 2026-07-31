3f66dcbec6876d46763c5b49c006672c6625a168
Marit Øye Gjersdal <mog@forskningsradet.no>
2026-07-31 09:49:56 +0000

    updated tests after changes to functions

--- tests/test_helpers_are_terms_in_input_text.py
+++ tests/test_helpers_are_terms_in_input_text.py
@@ -74,13 +74,15 @@ def test_are_terms_in_input_text(
     ]
     mocker_pattern_search_boolean.side_effect = [False, False]
 
-    output_excpected = {'Test1': False, 'Test2': False}
+    output_expected = {
+        'en': {'Test1': False, 'Test2': False}
+    }
 
     # Act
     output = _are_terms_in_input_text(termlists_en, input_text)
 
     # Assert
-    assert output == output_excpected
+    assert output == output_expected
 
 
 # Testcase: test that it calls _pattern_search_boolean with correct values
@@ -124,8 +126,18 @@ def test_are_terms_in_input_text_calls_prepare_regex_search_termlist(
     mocker_pattern_search_boolean.side_effect = [False, False]
 
     expected_calls_prepare_regex_search_termlist = [
-        mock.call(termlists_en[0], 'Text with UPPERCASE'),
-        mock.call(termlists_en[1], 'Text with UPPERCASE'),
+        mock.call(
+                terms=termlists_en[0]['wordlist_en'], 
+                input_text='Text with UPPERCASE', 
+                formatting_rule=termlists_en[0]['formatting_rule'], 
+                case=termlists_en[0]['case']
+        ),
+        mock.call(
+                terms=termlists_en[1]['wordlist_en'], 
+                input_text='Text with UPPERCASE', 
+                formatting_rule=termlists_en[1]['formatting_rule'], 
+                case=termlists_en[1]['case']
+        )
     ]
 
     # Act
@@ -136,49 +148,3 @@ def test_are_terms_in_input_text_calls_prepare_regex_search_termlist(
         expected_calls_prepare_regex_search_termlist
     )
     assert mocker_prepare_regex_search_termlist.call_count == 2
-
-
-# Testcase: multilanguage but english is present - to ensure that it still calls the function pattern_search_boolean
-@mock.patch('src.helpers.prepare_regex_search_termlist')
-@mock.patch('src.helpers._pattern_search_boolean')
-def test_are_terms_in_input_text_calls_prepare_regex_search_termlist_multilanguage(
-    mocker_pattern_search_boolean, mocker_prepare_regex_search_termlist
-):
-    mocker_prepare_regex_search_termlist.side_effect = [
-        ('(?:EN|To)', 'Text with UPPERCASE'),
-        ('(?:tre|fire)', 'text with uppercase'),
-    ]
-    mocker_pattern_search_boolean.side_effect = [False, False]
-
-    expected_calls_prepare_regex_search_termlist = [
-        mock.call(termlists_multilanguage[0], 'Text with UPPERCASE'),
-        mock.call(termlists_multilanguage[1], 'Text with UPPERCASE'),
-    ]
-
-    # Act
-    _are_terms_in_input_text(termlists_multilanguage, input_text)
-
-    # Assert
-    mocker_prepare_regex_search_termlist.assert_has_calls(
-        expected_calls_prepare_regex_search_termlist
-    )
-    assert mocker_prepare_regex_search_termlist.call_count == 2
-
-
-# Testcase: when all language lists are empty, does not call pattern_search_boolean and instead sets value to false
-@mock.patch('src.helpers.prepare_regex_search_termlist')
-@mock.patch('src.helpers._pattern_search_boolean')
-def test_are_terms_in_input_text_do_not_call_pattern_search_boolean_empty_termlists(
-    mocker_pattern_search_boolean, mocker_prepare_regex_search_termlist
-):
-    # Arrange
-    mocker_prepare_regex_search_termlist.side_effect = [
-        ('', 'Text with UPPERCASE'),
-        ('', 'text with uppercase'),
-    ]
-
-    # Act
-    _are_terms_in_input_text(termlists_empty, input_text)
-
-    # Assert
-    assert mocker_pattern_search_boolean.call_count == 0
